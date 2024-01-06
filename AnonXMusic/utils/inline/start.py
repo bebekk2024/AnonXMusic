ed5054d13b760e133a6ca655a7942cbcc0e0fee1
Wildan <155066169+Muhamadwildan018@users.noreply.github.com>
2024-01-07 00:01:16 +0700

    W start.py
    Signed-off-by: Wildan  <155066169+Muhamadwildan018@users.noreply.github.com>

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -10,7 +10,7 @@ def start_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_1"], url=f"https://t.me/Disney_storeDan"
             ),
-            InlineKeyboardButton(text=_["S_B_2"], url=f"https://t.me/+_5WmM2Mz4xc3MzQ1"),
+            InlineKeyboardButton(text=_["S_B_2"], url=f"https://t.me/musicsupport_dan"),
         ],
     ]
     return buttons
@@ -26,7 +26,7 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_5"], url=f"https://t.me/mhmdwldnnnn"),
-            InlineKeyboardButton(text=_["S_B_2"], url=f"https://t.me/+_5WmM2Mz4xc3MzQ1"),
+            InlineKeyboardButton(text=_["S_B_2"], url=f"https://t.me/musicsupport_dan"),
         ],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=f"https://t.me/Disney_storeDan"),
