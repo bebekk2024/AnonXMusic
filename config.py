94485daed867217955d521cecf8663c248c3c9c1
envinitysex <155812110+envinitysex@users.noreply.github.com>
2024-01-06 17:46:14 +0700

    E config.py
    Signed-off-by: envinitysex <155812110+envinitysex@users.noreply.github.com>

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/envynitysex/AnonXMusic",
+    "https://github.com/envinitysex/AnonXMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
