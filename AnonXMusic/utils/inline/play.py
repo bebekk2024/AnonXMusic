8815f2bed5f49dbe6cc611ebb6719a5a49116a1c
Wildan <155066169+Muhamadwildan018@users.noreply.github.com>
2024-01-07 10:19:39 +0700

    W play.py
    Signed-off-by: Wildan  <155066169+Muhamadwildan018@users.noreply.github.com>

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -60,7 +60,7 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text="✫ ꜱᴛᴏʀᴇ ✫", url=f"https://t.me/Disney_storeDan"),
         ],
         [
-            InlineKeyboardButton(text= "✫ ꜱᴜᴘᴘᴏʀᴛ ✫", url=f"https://t.me/musicsupport_dan"),
+            InlineKeyboardButton(text= "✫ ꜱᴜᴘᴘᴏʀᴛ ✫", url=f"https://t.me/musicsupportdan"),
         ],
     ]
     return buttons
@@ -73,7 +73,7 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="✫ ꜱᴛᴏʀᴇ ✫", url=f"https://t.me/Disney_storeDan"),
         ],
         [
-            InlineKeyboardButton(text= "✫ ꜱᴜᴘᴘᴏʀᴛ ✫", url=f"https://t.me/musicsupport_dan"),
+            InlineKeyboardButton(text= "✫ ꜱᴜᴘᴘᴏʀᴛ ✫", url=f"https://t.me/musicsupportdan"),
         ],
     ]
     return buttons
