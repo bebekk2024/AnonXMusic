b2a25e222537970f9239d479083cbad1cbcce443
Wildan <155066169+Muhamadwildan018@users.noreply.github.com>
2024-01-07 14:55:34 +0700

    W inlinequery.py
    Signed-off-by: Wildan  <155066169+Muhamadwildan018@users.noreply.github.com>

--- AnonXMusic/utils/inlinequery.py
+++ AnonXMusic/utils/inlinequery.py
@@ -7,31 +7,31 @@ answer.extend(
         InlineQueryResultArticle(
             title="Pᴀᴜsᴇ",
             description=f"ᴅᴀʜ ɢᴡ ᴊᴇᴅᴀ ʏᴀ ᴍᴇᴋ sᴛʀᴇᴀᴍ ɴʏᴀ.",
-            thumb_url="https://telegra.ph//file/ce83f87bc1ea1c3510ae2.jpg",
+            thumb_url="https://telegra.ph//file/cc3e25abe2c92af4e784c.jpg",
             input_message_content=InputTextMessageContent("/pause"),
         ),
         InlineQueryResultArticle(
             title="Rᴇsᴜᴍᴇ",
             description=f"ᴅᴀʜ ɢᴡ ʀᴇsᴜᴍᴇ ʏᴀ ᴍᴇᴋ sᴛʀᴇᴀᴍ ɴʏᴀ.",
-            thumb_url="https://telegra.ph//file/ce83f87bc1ea1c3510ae2.jpg",
+            thumb_url="https://telegra.ph//file/cc3e25abe2c92af4e784c.jpg",
             input_message_content=InputTextMessageContent("/resume"),
         ),
         InlineQueryResultArticle(
             title="Sᴋɪᴩ",
             description=f"ᴅᴀʜ ɢᴡ sᴋɪᴘ ʏᴀ ᴍᴇᴋ sᴛʀᴇᴀᴍ ɴʏᴀ.",
-            thumb_url="https://telegra.ph//file/ce83f87bc1ea1c3510ae2.jpg",
+            thumb_url="https://telegra.ph//file/cc3e25abe2c92af4e784c.jpg",
             input_message_content=InputTextMessageContent("/skip"),
         ),
         InlineQueryResultArticle(
             title="Eɴᴅ",
             description="ᴅᴀʜ ɢᴡ ᴇɴᴅ ʏᴀ ᴍᴇᴋ sᴛʀᴇᴀᴍ ɴʏᴀ.",
-            thumb_url="https://telegra.ph//file/ce83f87bc1ea1c3510ae2.jpg",
+            thumb_url="https://telegra.ph//file/cc3e25abe2c92af4e784c.jpg",
             input_message_content=InputTextMessageContent("/end"),
         ),
         InlineQueryResultArticle(
             title="Sʜᴜғғʟᴇ",
             description="ᴅᴀʜ ɢᴡ sᴜғғʟᴇ ʏᴀ ᴍᴇᴋ sᴛʀᴇᴀᴍ ɴʏᴀ.",
-            thumb_url="https://telegra.ph//file/ce83f87bc1ea1c3510ae2.jpg",
+            thumb_url="https://telegra.ph//file/cc3e25abe2c92af4e784c.jpg",
             input_message_content=InputTextMessageContent("/shuffle"),
         ),
         InlineQueryResultArticle(
