5a6409e1ea4cd733d4ef2e530fa7f8158dd27296
Wildan <146868572+Wildan2023@users.noreply.github.com>
2024-01-13 15:51:59 +0700

    inlinequery.py
    Signed-off-by: Wildan  <146868572+Wildan2023@users.noreply.github.com>

--- AnonXMusic/utils/inlinequery.py
+++ AnonXMusic/utils/inlinequery.py
@@ -7,37 +7,37 @@ answer.extend(
         InlineQueryResultArticle(
             title="Pᴀᴜsᴇ",
             description=f"ᴅᴀʜ ɢᴡ ᴊᴇᴅᴀ ʏᴀ ᴍᴇᴋ sᴛʀᴇᴀᴍ ɴʏᴀ.",
-            thumb_url="https://telegra.ph//file/c341a83ee4a8242b1ad45.jpg",
+            thumb_url="https://telegra.ph//file/30daa05aa0f61d5fb4714.jpg",
             input_message_content=InputTextMessageContent("/pause"),
         ),
         InlineQueryResultArticle(
             title="Rᴇsᴜᴍᴇ",
             description=f"ᴅᴀʜ ɢᴡ ʀᴇsᴜᴍᴇ ʏᴀ ᴍᴇᴋ sᴛʀᴇᴀᴍ ɴʏᴀ.",
-            thumb_url="https://telegra.ph//file/c341a83ee4a8242b1ad45.jpg",
+            thumb_url="https://telegra.ph//file/30daa05aa0f61d5fb4714.jpg",
             input_message_content=InputTextMessageContent("/resume"),
         ),
         InlineQueryResultArticle(
             title="Sᴋɪᴩ",
             description=f"ᴅᴀʜ ɢᴡ sᴋɪᴘ ʏᴀ ᴍᴇᴋ sᴛʀᴇᴀᴍ ɴʏᴀ.",
-            thumb_url="https://telegra.ph//file/c341a83ee4a8242b1ad45.jpg",
+            thumb_url="https://telegra.ph//file/30daa05aa0f61d5fb4714.jpg",
             input_message_content=InputTextMessageContent("/skip"),
         ),
         InlineQueryResultArticle(
             title="Eɴᴅ",
             description="ᴅᴀʜ ɢᴡ ᴇɴᴅ ʏᴀ ᴍᴇᴋ sᴛʀᴇᴀᴍ ɴʏᴀ.",
-            thumb_url="https://telegra.ph//file/c341a83ee4a8242b1ad45.jpg",
+            thumb_url="https://telegra.ph//file/30daa05aa0f61d5fb4714.jpg",
             input_message_content=InputTextMessageContent("/end"),
         ),
         InlineQueryResultArticle(
             title="Sʜᴜғғʟᴇ",
             description="ᴅᴀʜ ɢᴡ sᴜғғʟᴇ ʏᴀ ᴍᴇᴋ sᴛʀᴇᴀᴍ ɴʏᴀ.",
-            thumb_url="https://telegra.ph//file/c341a83ee4a8242b1ad45.jpg",
+            thumb_url="https://telegra.ph//file/30daa05aa0f61d5fb4714.jpg",
             input_message_content=InputTextMessageContent("/shuffle"),
         ),
         InlineQueryResultArticle(
             title="Lᴏᴏᴩ",
             description="ᴅᴀʜ ɢᴡ ʟᴏᴏᴘ ʏᴀ ᴍᴇᴋ sᴛʀᴇᴀᴍ ɴʏᴀ.",
-            thumb_url="https://telegra.ph//file/c341a83ee4a8242b1ad45.jpg",
+            thumb_url="https://telegra.ph//file/30daa05aa0f61d5fb4714.jpg",
             input_message_content=InputTextMessageContent("/loop 3"),
         ),
     ]
